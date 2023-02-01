4f2157f21c973706a570c0dd8fa845d78a851a35
Utkarsh721 <utkarshrai2121>
2023-02-02 02:41:32 +0530

    Resturent task1

---
+++ src/Component/Layout/Header.js
@@ -0,0 +1,19 @@
+import { Fragment } from "react"
+
+import HeaderCarButton from "./HeaderCartButton";
+import mealsImage from '../../assets/meals.jpg';
+import classes from './Header.module.css';
+
+const Header = props => {
+    return <Fragment>
+        <header className={classes.header}>
+            <h1>Romania Kitchen</h1>
+           <HeaderCarButton/>
+        </header>
+        <div className={classes['main-image']}>
+            <img src={mealsImage} alt="A table full of delicious food!" />
+        </div>
+    </Fragment>
+};
+
+export default Header;
