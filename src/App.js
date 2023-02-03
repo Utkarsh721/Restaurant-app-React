81bd60e9a540eb72ddd1d426a06df13b249fa456
Utkarsh721 <utkarshrai2121>
2023-02-03 10:09:20 +0530

    Task 2 Resturent Web

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import {Fragment} from 'react';
+import { Fragment } from 'react';
+
 import Header from "./Component/Layout/Header";
+import Meals from './Meals/Meals';
 
 function App() {
   return (
     <Fragment>
-    <Header/>
+      <Header />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 }
 
 export default App;
+
