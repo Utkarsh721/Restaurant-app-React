bcef6efad7674635746b455ea327145662005295
Utkarsh721 <utkarshrai2121>
2023-02-04 23:37:09 +0530

    Task 7 Resurent app

--- src/Component/Layout/HeaderCartButton.js
+++ src/Component/Layout/HeaderCartButton.js
@@ -5,11 +5,7 @@ import classes from './HeaderCartButton.module.css';
 import CartContext from "../../Store/cart-context";
 
 const HeaderCartButton = (props) => {
-  // const cartCtx = useContext(CartContext);
-
-  // const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
-  //   return curNumber + item.amount;
-  // }, 0);
+ 
  const cartCntxt = useContext(CartContext)
  let quantity=0;
  cartCntxt.items.forEach(item =>{
--- src/Meals/MealItem/MealItemForm.js
+++ src/Meals/MealItem/MealItemForm.js
@@ -9,25 +9,9 @@ const MealItemForm = (props) => {
   const addItemToCart = (event) => {
     event.preventDefault();
     const quantity = document.getElementById('amount' + props.id).value
-    // cartcntx.items.push(props.item)
     cartcntx.addItem({...props.item, quantity:quantity})
     console.log(' after addItemToCart', cartcntx);
   };
-  //  const [amountIsValid,setAmountISValid]= useState(true)
-  // const amountInputRef=useRef();
-
-  //  const submitHandler=(event) =>{
-  //    event.preventDefault();
-
-  //    const enteredAmount=amountInputRef.current.value;
-  //    const enteredAmountNumber = +enteredAmount;
-
-  // if(enteredAmount.trim().length ===0 || enteredAmountNumber < 1 ||enteredAmountNumber >5) {
-  //   setAmountISValid(false);
-  //   return;
-  // }
-
-
   return (
     <form className={classes.form} >
       {console.log('inside render', cartcntx.items)}
@@ -40,7 +24,6 @@ const MealItemForm = (props) => {
         defaultValue: '1'
       }} />
       <button onClick={addItemToCart} >+ Add</button>
-      {/* {!amountIsValid && <p>Please enter a valid amount (1-5).</p>} */}
 
     </form>
   );
